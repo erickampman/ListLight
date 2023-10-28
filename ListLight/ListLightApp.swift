09620d289c63cd888bb0e66455a1b95188dfab1e
Eric Kampman <erickampman@yahoo.com>
2023-10-28 06:54:51 -0700

    Initial Commit

---
+++ ListLight/ListLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListLightApp.swift
+//  ListLight
+//
+//  Created by Eric Kampman on 10/28/23.
+//
+
+import SwiftUI
+
+@main
+struct ListLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
